fa98dc2766128f78aa9aec272c0ab7c2defd74a8
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-06-15 01:14:20 +0900

    wip

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var maxAttemptTimes = 5;
 var GITHUB_EASY_EDIT_BUTTON_CLASS_NAME = 'github-easy-edit-button'
 
 // editButtonが取得できたりできなかったりしたため複数回試行している
-function loop(i) {
+function createButton(i) {
   var hasGotEditButton = false;
   var attemptCount = 0;
-  console.log({ i, attemptCount, hasGotEditButton })
   setTimeout(function() {
     var editButton = document.getElementsByClassName('f5 text-normal')[i].parentNode.getElementsByClassName('dropdown-item btn-link js-comment-edit-button')[0]
+    console.log({ i, attemptCount, hasGotEditButton })
 
     if (editButton != undefined) {
       var easyEditButton = editButton.cloneNode(true);
@@ -17,15 +17,14 @@ function loop(i) {
       hasGotEditButton = true;
     } else if (attemptCount < (maxAttemptTimes - 1) || !hasGotEditButton) {
       attemptCount++;
-      setTimeout(loop(i), 0);
+      setTimeout(function() { createButton(i) }, 0);
     }
-  }, 100);
+  }, 1000);
 }
 
-// FIXME: リロード・リンク直叩きしないと動かない https://github.com/kyu08/github-easy-edit/issues/2
 for (let i = 0; i < document.getElementsByClassName('f5 text-normal').length; i++) {
   var actionButton = document.getElementsByClassName('f5 text-normal')[i].parentNode.getElementsByClassName('timeline-comment-action Link--secondary Button--link Button--medium Button')[0];
   actionButton.click();
-  loop(i);
+  createButton(i);
   actionButton.click();
 }
